4d69554cd1e52f0bbe015776103123c785dcc377
ashutosh <ashutoshkrm@gmail.com>
2023-12-21 14:06:11 +0530

    updated code for hub-spoke network

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-# Azurerm provider configuration
-# provider "azurerm" {
-#   features {}
-# }
-
 module "vnet-hub" {
   source  = "./modules/terraform-azure-virtual-hub-network/"
   # version = "2.2.0"
@@ -124,6 +119,106 @@ module "vnet-hub" {
 
   # Adding TAG's to your Azure resources (Required)
   # ProjectName and Env are already declared above, to use them here, create a varible. 
+  tags = {
+    ProjectName  = "CR0068"
+    Env          = "prod"
+    Owner        = "ashutoshkrm@gmail.com"
+    BusinessUnit = "CORP"
+    ServiceClass = "Gold"
+  }
+}
+
+data "azurerm_virtual_network" "hub-vnet" {
+  name                = "vnet-default-hub-eastus"
+  resource_group_name = "rg-hub-vnet-eus-001"
+}
+
+# data "azurerm_storage_account" "hub-st" {
+#   name                = "stdiaglogs3hfeutdh"
+#   resource_group_name = "rg-hub-demo-internal-shared-westeurope-001"
+# }
+
+# data "azurerm_log_analytics_workspace" "hub-logws" {
+#   name                = "logaws-3hfeutdh-default-hub-westeurope"
+#   resource_group_name = "rg-hub-vnet-eus-001"
+# }
+
+module "vnet-spoke" {
+  source  = "./modules/terraform-azure-virtual-spoke-network"
+ # version = "2.2.0"
+
+  # By default, this module will create a resource group, proivde the name here
+  # to use an existing resource group, specify the existing resource group name,
+  # and set the argument to `create_resource_group = false`. Location will be same as existing RG.
+  resource_group_name = "rg-spoke-vnet-eus-001"
+  location            = "eastus"
+  spoke_vnet_name     = "corp-spoke"
+
+  # Specify if you are deploying the spoke VNet using the same hub Azure subscription
+  is_spoke_deployed_to_same_hub_subscription = true
+
+  # Provide valid VNet Address space for spoke virtual network.  
+  vnet_address_space = ["10.2.0.0/16"]
+
+  # Hub network details to create peering and other setup
+  hub_virtual_network_id          = data.azurerm_virtual_network.hub-vnet.id
+  # hub_firewall_private_ip_address = "10.1.0.4"
+  # private_dns_zone_name           = "privatecloud.exashutoshkrmample.com"
+  # hub_storage_account_id          = data.azurerm_storage_account.hub-st.id
+
+  # (Required) To enable Azure Monitoring and flow logs
+  # pick the values for log analytics workspace which created by Hub module
+  # Possible values range between 30 and 730
+  # log_analytics_workspace_id           = data.azurerm_log_analytics_workspace.hub-logws.id
+  # log_analytics_customer_id            = data.azurerm_log_analytics_workspace.hub-logws.workspace_id
+  # log_analytics_logs_retention_in_days = 30
+
+  # Multiple Subnets, Service delegation, Service Endpoints, Network security groups
+  # These are default subnets with required configuration, check README.md for more details
+  # Route_table and NSG association to be added automatically for all subnets listed here.
+  # subnet name will be set as per Azure naming convention by defaut. expected value here is: <App or project name>
+  subnets = {
+
+    app_subnet = {
+      subnet_name           = "applicaiton"
+      subnet_address_prefix = ["10.2.1.0/24"]
+      service_endpoints     = ["Microsoft.Storage"]
+
+      nsg_inbound_rules = [
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+        # To use defaults, use "" without adding any value and to use this subnet as a source or destination prefix.
+        ["ssh", "200", "Inbound", "Allow", "Tcp", "22", "*", ""],
+        ["rdp", "201", "Inbound", "Allow", "Tcp", "3389", "*", ""],
+      ]
+
+      nsg_outbound_rules = [
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+        # To use defaults, use "" without adding any value and to use this subnet as a source or destination prefix.
+        ["ntp_out", "203", "Outbound", "Allow", "Udp", "123", "", "0.0.0.0/0"],
+      ]
+    }
+
+    db_subnet = {
+      subnet_name           = "database"
+      subnet_address_prefix = ["10.2.2.0/24"]
+      service_endpoints     = ["Microsoft.Storage"]
+      nsg_inbound_rules = [
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+        # To use defaults, use "" without adding any value and to use this subnet as a source or destination prefix.
+        ["http", "100", "Inbound", "Allow", "Tcp", "80", "*", "0.0.0.0/0"],
+        ["sql_port", "101", "Inbound", "Allow", "Tcp", "1433", "*", ""],
+      ]
+
+      nsg_outbound_rules = [
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+        # To use defaults, use "" without adding any value and to use this subnet as a source or destination prefix.
+        ["ntp_out", "102", "Outbound", "Allow", "Udp", "123", "", "0.0.0.0/0"],
+      ]
+    }
+  }
+
+  # Adding TAG's to your Azure resources (Required)
+  # ProjectName and Env are already declared above, to use them here, create a varible.
   tags = {
     ProjectName  = "CR0068"
     Env          = "prod"
